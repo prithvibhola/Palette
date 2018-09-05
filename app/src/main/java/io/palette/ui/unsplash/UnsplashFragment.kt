546d7efc085ec80c050aa9b36bc731dad12f0887
prithvi <prithvibhola08@gmail.com>
2018-09-05 17:08:51 +0530

    Code clean

--- app/src/main/java/io/palette/ui/unsplash/UnsplashFragment.kt
+++ app/src/main/java/io/palette/ui/unsplash/UnsplashFragment.kt
@@ -1,63 +1,57 @@
 package io.palette.ui.unsplash
 
-import android.arch.lifecycle.Observer
 import android.arch.lifecycle.ViewModelProvider
 import android.os.Bundle
-import android.support.v7.widget.LinearLayoutManager
 import android.support.v7.widget.StaggeredGridLayoutManager
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import io.palette.R
 import io.palette.data.models.Response
-import io.palette.data.models.Unsplash
 import io.palette.di.FragmentScoped
-import io.palette.utility.extentions.getViewModel
 import io.palette.ui.base.BaseFragment
-import io.palette.viewmodel.UnsplashViewModel
+import io.palette.utility.extentions.getViewModel
+import io.palette.utility.extentions.observe
 import kotlinx.android.synthetic.main.fragment_unsplash.*
 import javax.inject.Inject
 
 @FragmentScoped
 class UnsplashFragment @Inject constructor() : BaseFragment() {
 
-    @Inject
-    lateinit var viewModelFactory: ViewModelProvider.Factory
+    @Inject lateinit var viewModelFactory: ViewModelProvider.Factory
 
+    private lateinit var viewModel: UnsplashViewModel
     private lateinit var mAdapter: UnsplashAdapter
-    lateinit var viewModel: UnsplashViewModel
 
     companion object {
         fun newInstance() = UnsplashFragment()
     }
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?) =
-            inflater.inflate(R.layout.fragment_unsplash, container, false)!!
+            inflater.inflate(R.layout.fragment_unsplash, container, false)
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        viewModel = getViewModel(UnsplashViewModel::class.java, viewModelFactory)
-        setUpRecyclerView()
-        viewModel.unsplash.observe(this, Observer { mAdapter.submitList(it) })
-        viewModel.getNetworkState().observe(this, Observer<Response<Unsplash>> { mAdapter.setNetworkState(it) })
-        viewModel.getRefreshState().observe(this, Observer { setInitialLoadingState(it) })
-    }
 
-    private fun setUpRecyclerView() {
-        mAdapter = UnsplashAdapter(requireContext(), { viewModel.retry() })
+        viewModel = getViewModel(UnsplashViewModel::class.java, viewModelFactory)
+
+        mAdapter = UnsplashAdapter(requireContext()) { viewModel.retry() }
         rvUnsplash.apply {
             layoutManager = StaggeredGridLayoutManager(2, StaggeredGridLayoutManager.VERTICAL);
             adapter = mAdapter
         }
-    }
 
-    private fun setInitialLoadingState(state: Response<Unsplash>?) {
-        when (state?.status) {
-            Response.Status.SUCCESS -> {
-            }
-            Response.Status.LOADING -> {
-            }
-            Response.Status.ERROR -> {
+        observe(viewModel.unsplash) { mAdapter.submitList(it) }
+        observe(viewModel.getNetworkState()) { mAdapter.setNetworkState(it) }
+        observe(viewModel.getRefreshState()) {
+            it ?: return@observe
+            when (it.status) {
+                Response.Status.SUCCESS -> {
+                }
+                Response.Status.LOADING -> {
+                }
+                Response.Status.ERROR -> {
+                }
             }
         }
     }
