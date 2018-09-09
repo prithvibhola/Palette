122cffffc393a00ee3011aa73ca421638b3f0afa
prithvi <prithvibhola08@gmail.com>
2018-09-09 10:35:16 +0530

    Firebase login using FirebaseUI

--- app/src/main/java/io/palette/ui/profile/ProfileFragment.kt
+++ app/src/main/java/io/palette/ui/profile/ProfileFragment.kt
@@ -1,17 +1,14 @@
 package io.palette.ui.profile
 
+import android.app.Activity.RESULT_OK
 import android.arch.lifecycle.ViewModelProvider
 import android.content.Intent
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import com.google.android.gms.auth.api.Auth
-import com.google.android.gms.auth.api.signin.GoogleSignIn
-import com.google.android.gms.auth.api.signin.GoogleSignInClient
-import com.google.android.gms.auth.api.signin.GoogleSignInOptions
-import com.google.android.gms.common.api.ApiException
-import com.google.android.gms.common.api.GoogleApiClient
+import com.firebase.ui.auth.AuthUI
+import com.firebase.ui.auth.IdpResponse
 import com.google.firebase.auth.FirebaseAuth
 import io.palette.R
 import io.palette.data.models.Response
@@ -25,12 +22,11 @@ import kotlinx.android.synthetic.main.fragment_profile.*
 import timber.log.Timber
 import javax.inject.Inject
 
+
 @FragmentScoped
 class ProfileFragment @Inject constructor() : BaseFragment() {
 
     @Inject lateinit var mAuth: FirebaseAuth
-    @Inject lateinit var mGoogleSignInClient: GoogleSignInClient
-    private var mGoogleApiClient: GoogleApiClient? = null
     @Inject lateinit var viewModelFactory: ViewModelProvider.Factory
 
     lateinit var viewModel: ProfileViewModel
@@ -47,8 +43,6 @@ class ProfileFragment @Inject constructor() : BaseFragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-//        Firebase.setAndroidContext(activity)
-
         viewModel = getViewModel(ProfileViewModel::class.java, viewModelFactory)
 
         observe(viewModel.user) {
@@ -60,23 +54,19 @@ class ProfileFragment @Inject constructor() : BaseFragment() {
             }
         }
 
-        //GOOGLE
-        val gso = GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
-                .requestIdToken(getString(R.string.default_web_client_id))
-                .requestEmail()
-                .build()
-        mGoogleApiClient = GoogleApiClient.Builder(context!!)
-                .addApi(Auth.GOOGLE_SIGN_IN_API, gso)
-                .build()
-
-        if (mAuth.currentUser == null) {
-            btnLogin.visible = true
-            btnLogin.setOnClickListener {
-                val signInIntent = Auth.GoogleSignInApi.getSignInIntent(mGoogleApiClient)
-                startActivityForResult(signInIntent, RC_SIGN_IN)
+        when (mAuth.currentUser == null) {
+            true -> {
+                btnLogin.visible = true
+                btnLogin.setOnClickListener {
+                    startActivityForResult(
+                            AuthUI.getInstance()
+                                    .createSignInIntentBuilder()
+                                    .setAvailableProviders(listOf(AuthUI.IdpConfig.GoogleBuilder().build()))
+                                    .build(),
+                            RC_SIGN_IN)
+                }
             }
-        } else {
-            btnLogin.visible = false
+            false -> btnLogin.visible = false
         }
     }
 
@@ -84,24 +74,13 @@ class ProfileFragment @Inject constructor() : BaseFragment() {
         super.onActivityResult(requestCode, resultCode, data)
 
         if (requestCode == RC_SIGN_IN) {
-            val result = Auth.GoogleSignInApi.getSignInResultFromIntent(data)
-            if (result.isSuccess) {
-                val account = result.signInAccount
-                viewModel.login(requireActivity(), account!!)
+            val response = IdpResponse.fromResultIntent(data)
+            response ?: return
+            if (resultCode == RESULT_OK) {
+                val user = FirebaseAuth.getInstance().currentUser
+            } else {
+                Timber.e(response.error, "User could not sign in")
             }
         }
-    }
-
-    override fun onStart() {
-        super.onStart()
-        if (mGoogleApiClient != null)
-            mGoogleApiClient?.connect()
-    }
-
-    override fun onStop() {
-        if (mGoogleApiClient != null && mGoogleApiClient?.isConnected!!) {
-            mGoogleApiClient?.disconnect()
-        }
-        super.onStop()
     }
 }
