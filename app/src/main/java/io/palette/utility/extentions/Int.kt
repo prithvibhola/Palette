8481a8c083083ca53c8aa8bb8e4b65238609b3f9
prithvi <prithvibhola08@gmail.com>
2018-09-25 14:29:55 +0530

    Settings screen UI and random method update

--- app/src/main/java/io/palette/utility/extentions/Int.kt
+++ app/src/main/java/io/palette/utility/extentions/Int.kt
@@ -5,6 +5,6 @@ import java.util.*
 
 fun Int.toHex(): String = Integer.toHexString(this)
 
-fun List<Int>.getRandom() = this[Random().nextInt(this.size - 1)]
+fun List<Int>.getRandom() = this[Random().nextInt(this.size)]
 
 fun Int.rgbString(): String = "(${Color.red(this@rgbString)}, ${Color.green(this@rgbString)}, ${Color.blue(this@rgbString)})"
