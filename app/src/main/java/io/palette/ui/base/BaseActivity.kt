b68ad6e227091e8e4cdba7321e0e24f6e2ef5034
prithvi <prithvibhola08@gmail.com>
2018-09-25 14:29:55 +0530

    Settings screen UI and random method update

--- app/src/main/java/io/palette/ui/base/BaseActivity.kt
+++ app/src/main/java/io/palette/ui/base/BaseActivity.kt
@@ -2,11 +2,22 @@ package io.palette.ui.base
 
 import android.os.Bundle
 import android.os.PersistableBundle
+import android.view.MenuItem
 import dagger.android.support.DaggerAppCompatActivity
 
 abstract class BaseActivity : DaggerAppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?, persistentState: PersistableBundle?) {
         super.onCreate(savedInstanceState, persistentState)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return when (item.itemId) {
+            android.R.id.home -> {
+                onBackPressed()
+                true
+            }
+            else -> super.onOptionsItemSelected(item)
+        }
     }
 }
