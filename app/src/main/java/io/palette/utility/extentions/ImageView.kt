1d0383ed256ee564dd2cb1c72b990f49495eb8ef
prithvi <prithvibhola08@gmail.com>
2018-09-25 13:43:55 +0530

    ViewPager page change UI handled

--- app/src/main/java/io/palette/utility/extentions/ImageView.kt
+++ app/src/main/java/io/palette/utility/extentions/ImageView.kt
@@ -1,8 +1,17 @@
 package io.palette.utility.extentions
 
+import android.support.annotation.ColorRes
+import android.support.annotation.DrawableRes
+import android.support.v4.content.ContextCompat
+import android.widget.ImageView
 import io.palette.utility.DynamicHeightImageView
 
 fun DynamicHeightImageView.maintainAspectRatio(width: Long, height: Long) {
     val aspectRatio = (width / height).toFloat()
     this.setAspectRatio(if (aspectRatio <= 0.0) 1.5F else aspectRatio)
 }
+
+fun ImageView.setColor(@ColorRes color: Int) = this.setColorFilter(ContextCompat.getColor(context, color))
+
+fun ImageView.setImage(@DrawableRes image: Int) = this.setImageDrawable(ContextCompat.getDrawable(context, image))
+
