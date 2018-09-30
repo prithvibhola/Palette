c760a62b31724b179f6b954a5a70d43d5619e226
prithvi <prithvibhola08@gmail.com>
2018-09-30 19:57:06 +0530

    About screen and icon

--- app/src/main/java/io/palette/ui/about/AboutActivity.kt
+++ app/src/main/java/io/palette/ui/about/AboutActivity.kt
@@ -2,10 +2,13 @@ package io.palette.ui.about
 
 import android.content.Context
 import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
 import android.os.PersistableBundle
 import io.palette.R
 import io.palette.ui.base.BaseActivity
+import io.palette.utility.extentions.setScreenTitle
+import kotlinx.android.synthetic.main.activity_about.*
 
 class AboutActivity : BaseActivity() {
 
@@ -13,8 +16,13 @@ class AboutActivity : BaseActivity() {
         fun newInstance(context: Context) = Intent(context, AboutActivity::class.java)
     }
 
-    override fun onCreate(savedInstanceState: Bundle?, persistentState: PersistableBundle?) {
-        super.onCreate(savedInstanceState, persistentState)
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_about)
+
+        setSupportActionBar(toolbar)
+        supportActionBar?.setScreenTitle(getString(R.string.title_about))
+
+        tvGithubLink.setOnClickListener { startActivity(Intent(Intent.ACTION_VIEW, Uri.parse("https://github.com/prithvibhola/Palette"))) }
     }
 }
